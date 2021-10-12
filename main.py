0e09ead7a8463b23f720033ee95220606ece2bd1
Bekyy <51543747+Bekyy@users.noreply.github.com>
2021-10-12 00:15:33 -0900

    Update main.py

--- main.py
+++ main.py
@@ -1,76 +1,39 @@
-# import os
+import time
 import telebot
-import yfinance as yf
-from datetime import datetime
 
-# API_KEY = os.getenv('API_KEY')
-# my_secret = os.environ['API_KEY']
 TOKEN = "2023190356:AAEFaG-q5vPnlyXzYu8Uq0QH9yUQj8W59rQ"
 bot = telebot.TeleBot(token=TOKEN)
 
-@bot.message_handler(commands=['start'])
-def start(message):
-  bot.reply_to(message, "Hey I am Beky's Tutorial Bot")
+def findat(msg):
+    # from a list of texts, it finds the one with the '@' sign
+    for i in msg:
+        if '@' in i:
+            return i
 
-@bot.message_handler(commands=['Greet'])
-def greet(message):
-  bot.reply_to(message, "Hey there! Hows it going?")
+@bot.message_handler(commands=['start']) # welcome message handler
+def send_welcome(message):
+    bot.reply_to(message, 'Hey I am Beky's Tutorial Bot')
 
-@bot.message_handler(commands=['hello'])
-def hello(message):
-  bot.send_message(message.chat.id, "Hello!")
+@bot.message_handler(commands=['help']) # help message handler
+def send_welcome(message):
+    bot.reply_to(message, 'ALPHA = FEATURES MAY NOT WORK')
 
-@bot.message_handler(commands=['date'])
-def date(message):
-  now = datetime.now()
-  date_time = now.strftime('%m/%d %I:%M %p')
-  bot.send_message(message.chat.id, date_time)  
+@bot.message_handler(func=lambda msg: msg.text is not None and '@' in msg.text)
+# lambda function finds messages with the '@' sign in them
+# in case msg.text doesn't exist, the handler doesn't process it
+def at_converter(message):
+    texts = message.text.split()
+    at_text = findat(texts)
+    if at_text == '@': # in case it's just the '@', skip
+        pass
+    else:
+        insta_link = "https://instagram.com/{}".format(at_text[1:])
+        bot.reply_to(message, insta_link)
 
-@bot.message_handler(commands=['wsb'])
-def get_stocks(message):
-  response = ""
-  stocks = ['gme', 'amc', 'nok']
-  stock_data = []
-  for stock in stocks:
-    data = yf.download(tickers=stock, period='2d', interval='1d')
-    data = data.reset_index()
-    response += f"-----{stock}-----\n"
-    stock_data.append([stock])
-    columns = ['stock']
-    for index, row in data.iterrows():
-      stock_position = len(stock_data) - 1
-      price = round(row['Close'], 2)
-      format_date = row['Date'].strftime('%m/%d')
-      response += f"{format_date}: {price}\n"
-      stock_data[stock_position].append(price)
-      columns.append(format_date)
-    print()
-
-  response = f"{columns[0] : <10}{columns[1] : ^10}{columns[2] : >10}\n"
-  for row in stock_data:
-    response += f"{row[0] : <10}{row[1] : ^10}{row[2] : >10}\n"
-  response += "\nStock Data"
-  print(response)
-  bot.send_message(message.chat.id, response)
-
-def stock_request(message):
-  request = message.text.split()
-  if len(request) < 2 or request[0].lower() not in "price":
-    return False
-  else:
-    return True
-
-@bot.message_handler(func=stock_request)
-def send_price(message):
-  request = message.text.split()[1]
-  data = yf.download(tickers=request, period='5m', interval='1m')
-  if data.size > 0:
-    data = data.reset_index()
-    data["format_date"] = data['Datetime'].dt.strftime('%m/%d %I:%M %p')
-    data.set_index('format_date', inplace=True)
-    print(data.to_string())
-    bot.send_message(message.chat.id, data['Close'].to_string(header=False))
-  else:
-    bot.send_message(message.chat.id, "No data!?")
-
-bot.polling()
+while True:
+    try:
+        bot.polling(none_stop=True)
+        # ConnectionError and ReadTimeout because of possible timout of the requests library
+        # maybe there are others, therefore Exception
+    except Exception:
+        time.sleep(15)
